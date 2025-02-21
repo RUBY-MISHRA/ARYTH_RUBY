f97c180849841ad899387475115ed8f329f26732
RUBY-MISHRA <rubyfet@kiit.ac.in>
2025-02-21 21:27:11 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -20,8 +20,8 @@ module tb ();
   reg [7:0] ui_in;
   reg [7:0] uio_in;
   wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+//  wire [7:0] uio_out;
+//  wire [7:0] uio_oe;
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
@@ -39,8 +39,8 @@ module tb ();
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+     // .uio_out(uio_out),  // IOs: Output path
+    //  .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
